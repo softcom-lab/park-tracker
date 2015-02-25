11319b088051553bbd943c47de26c54c699b4581
Stevo <smsairafian@csupomona.edu>
2015-02-24 21:49:21 -0800

    Merge branch 'master' of https://github.com/StevenSairafian/demo-web-project.git
    Working on fixing file restructuring caused by bad commit

---
+++ cs480/src/main/java/io/stevensairafian/github/util/ResourceResolver.java
@@ -0,0 +1,27 @@
+package io.stevensairafian.github.util;
+
+import java.io.File;
+
+/**
+ * This is an utility class to help file locating.
+ */
+public class ResourceResolver {
+
+	/** The base folder to store all the data used by this project. */
+	private static final String BASE_DIR = System.getProperty("user.home")
+			+ "/parkTracker";
+
+	/**
+	 * Get the file used to store the user object JSON
+	 *
+	 * @param userId
+	 * @return
+	 */
+	public static File getThemeParkFile() {
+		File file = new File(BASE_DIR + "/" + "park-map.json");
+		if (!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
+		}
+		return file;
+	}
+}
--- src/main/java/io/stevensairafian/github/data/ParkDay.java
+++ src/main/java/io/stevensairafian/github/data/ParkDay.java
@@ -1,6 +1,8 @@
 package io.stevensairafian.github.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ParkDay {
 	private HashMap<String, Attraction> attractions = new HashMap<String, Attraction>();
@@ -11,5 +13,17 @@ public class ParkDay {
 
 	public void setAttractions(HashMap<String, Attraction> attractions) {
 		this.attractions = attractions;
+	}
+	
+	public void addAttraction(Attraction attraction){
+		attractions.put(attraction.getAttractionId(), attraction);
+	}
+	
+	public void updateAttraction(Attraction attraction){
+		attractions.put(attraction.getAttractionId(), attraction);
+	}
+	
+	public List<Attraction> listAllAttractions() {
+		return new ArrayList<Attraction>(attractions.values());
 	}
 }
